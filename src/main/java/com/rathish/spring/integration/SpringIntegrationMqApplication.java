25d83fda6412fe2a44603dced24728aabe4d1cb3
Rathishkumar Nair <rathishks@gmail.com>
2018-08-31 22:33:07 -0400

    Initial Commit

---
+++ src/main/java/com/rathish/spring/integration/SpringIntegrationMqApplication.java
@@ -0,0 +1,12 @@
+package com.rathish.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationMqApplication.class, args);
+    }
+}
